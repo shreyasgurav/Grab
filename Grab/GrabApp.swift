9d64b7fb6e336019de657b206a50405efbaaee9a
Shreyas Gurav <shrreyasgurav@gmail.com>
2025-12-28 10:07:26 +0530

    Initial Commit

---
+++ Grab/GrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabApp.swift
+//  Grab
+//
+//  Created by Shreyas Gurav on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrabTests/GrabTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrabTests.swift
+//  GrabTests
+//
+//  Created by Shreyas Gurav on 28/12/25.
+//
+
+import Testing
+@testable import Grab
+
+struct GrabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
